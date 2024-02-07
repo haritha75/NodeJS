2845027f3a6552849dfc8bd638ff9b86870cecd7
Yerukondu Haritha <yerukonduharitha@gmail.com>
2024-02-07 20:52:32 +0530

    Day5 work done

---
+++ Day5/ExpressMiddleware.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+function requestLoggerMiddleware(req, res, next) {
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  console.log(`${timestamp} - ${method} request received.`);
+  next();
+}
+
+app.use(requestLoggerMiddleware);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3000, () => {
+  console.log("server is running on the port 3000");
+});
